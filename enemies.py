0380e4e0f229b1ff95385068ce7f9dab011d640a
IzaKastra <87741876+IzaKastra@users.noreply.github.com>
2025-03-04 15:58:09 -0500

    Reformat PR to match gear.py style
    Essentially reverted the PR to the original code, but made use of the compressed format and list comprehension to make it easier for users to add new enemies in the same way they add gear to gear.py

--- enemies.py
+++ enemies.py
@@ -1,35 +1,35 @@
-def create_enemy(name, level, defense, evasion, vit, agi, mnd, int, chr, location, chr_comment=None, agi_comment=None):
-    enemy = {
-        'Name': name,
-        'Level': level,
-        'Defense': defense,
-        'Evasion': evasion,
-        'VIT': vit,
-        'AGI': agi,
-        'MND': mnd,
-        'INT': int,
-        'CHR': chr,
-        'Magic Defense': 0,
-        'Magic Evasion': 0,
-        'Location': location
-    }
-    if chr_comment:
-        enemy['CHR'] = f"{chr}  # {chr_comment}"
-    if agi_comment:
-        enemy['AGI'] = f"{agi}  # {agi_comment}"
-    return enemy
+import sys
+
+# Enemy information taken from https://w.atwiki.jp/bartlett3/pages/327.html
+
+# Add new enemies to the default list by copy+pasting a pre-existing entry and adjusting the values.
 
 enemies = [
-    ("Apex Bats", 129, 1142, 1043, 254, 298, 233, 233, 247, "Dho Gates"),
-    ("Apex Toad", 132, 1239, 1133, 270, 348, 224, 293, 277, "Woh Gates"),
-    ("Apex Bat", 135, 1338, 1224, 289, 340, 267, 267, 282, "Outer Ra'Kaznar"),
-    ("Apex Lugcrawler Hunter", 138, 1446, 1314, 325, 346, 284, 284, 284, "Crawler's Nest [S]", "CHR unknown. Copied MND/INT"),
-    ("Apex Knight Lugcrawler", 140, 1530, 1383, 356, 343, 297, 297, 297, "Crawler's Nest [S]", "CHR unknown. Copied MND/INT"),
-    ("Apex Idle Drifter", 142, 1448, 1502, 348, 366, 327, 327, 327, "Promyvion", "CHR unknown. Copied MND/INT"),
-    ("Apex Archaic Cog", 145, 1704, 1551, 381, 440, 353, 365, 353, "Alzadaal Undersea Ruins", "CHR unknown. Copied MND"),
-    ("Apex Archaic Cogs", 147, 1791, 1628, 399, 443, 377, 390, 377, "Alzadaal Undersea Ruins", "CHR unknown. Copied MND"),
-    ("Ozma", 999, 9999, 9999, 999, 999, 999, 999, 999, "Chocobo's Air Garden", "CHR unknown. Using value from apex_toad", "AGI unknown. Using value from apex_toad"),
-    ("Octorok", 1, 1, 1, 1, 1, 1, 1, 1, "Hyrule")
-]
+    {"Name":"Apex Bats", "Level":129, "Defense":1142, "Evasion":1043, "VIT":254, "AGI":298, "MND":233, "INT":233, "CHR":247, "Magic Evasion":0, "Magic Defense":0, "Location":"Dho Gates"},
+    {"Name":"Apex Toad", "Level":132, "Defense":1239, "Evasion":1133, "VIT":270, "AGI":348, "MND":224, "INT":293, "CHR":277, "Magic Evasion":0, "Magic Defense":0, "Location":"Woh Gates"}, 
+    {"Name":"Apex Bat", "Level":135, "Defense":1338, "Evasion":1224, "VIT":289, "AGI":340, "MND":267, "INT":267, "CHR":282, "Magic Evasion":0, "Magic Defense":0, "Location":"Outer Ra'Kaznar"},
+    {"Name":"Apex Lugcrawler Hunter", "Level":138, "Defense":1446, "Evasion":1314, "VIT":325, "AGI":346, "MND":284, "INT":284, "CHR":284, "Magic Evasion":0, "Magic Defense":0, "Location":"Crawler's Nest [S]",}, # "CHR unknown. Copied MND,INT
+    {"Name":"Apex Knight Lugcrawler", "Level":140, "Defense":1530, "Evasion":1383, "VIT":356, "AGI":343, "MND":297, "INT":297, "CHR":297, "Magic Evasion":0, "Magic Defense":0, "Location":"Crawler's Nest [S]"}, # CHR unknown. Copied MND,INT
+    {"Name":"Apex Idle Drifter", "Level":142, "Defense":1448, "Evasion":1502, "VIT":348, "AGI":366, "MND":327, "INT":327, "CHR":327, "Magic Evasion":0, "Magic Defense":0, "Location":"Promyvion"}, # CHR unknown. Copied MND, INT
+    {"Name":"Apex Archaic Cog", "Level":145, "Defense":1704, "Evasion":1551, "VIT":381, "AGI":440, "MND":353, "INT":365, "CHR":353, "Magic Evasion":0, "Magic Defense":0, "Location":"Alzadaal Undersea Ruins"},  # CHR unknown. Copied MND
+    {"Name":"Apex Archaic Cogs", "Level":147, "Defense":1791, "Evasion":1628, "VIT":399, "AGI":443, "MND":377, "INT":390, "CHR":377, "Magic Evasion":0, "Magic Defense":0, "Location":"Alzadaal Undersea Ruins"},  # CHR unknown. Copied MND
+    {"Name":"Ozma", "Level":999, "Defense":9999, "Evasion":9999, "VIT":999, "AGI":999, "MND":999, "INT":999, "CHR":999, "Magic Evasion":0, "Magic Defense":0, "Location":"Chocobo's Air Garden"},
+    {"Name":"Octorok", "Level":1, "Defense":1, "Evasion":1, "VIT":1, "AGI":1, "MND":1, "INT":1, "CHR":1, "Magic Evasion":0, "Magic Defense":0, "Location":"Hyrule"},
+    ]
 
-preset_enemies = {name: create_enemy(name, level, defense, evasion, vit, agi, mnd, int, chr, location, chr_comment) for name, level, defense, evasion, vit, agi, mnd, int, chr, location, *chr_comment in enemies}
+
+
+
+
+# Do not modify below this line.
+
+# Check that all necessary stats are included in all enemies
+missing_stat = False
+for enemy in enemies:
+    for key in ["Name", "Level", "Defense", "Evasion", "VIT", "AGI", "MND", "INT", "CHR", "Magic Evasion", "Magic Defense", "Location"]: 
+        if enemy.get(key, False) is False:
+            print(f"Missing stat \"{key}\" for {enemy['Name']}. Update the enemies.py file to include this stat.")
+            missing_stat = True
+if missing_stat:
+    sys.exit()
+preset_enemies = {k["Name"]:k for k in sorted(enemies, key=lambda x:x["Level"])}
