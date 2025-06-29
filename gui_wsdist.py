ea35d85c7b52812b84069818554de397188f4fa1
IzaKastra <87741876+IzaKastra@users.noreply.github.com>
2025-06-29 15:04:23 -0400

    Remove multi-counting of Hover Shot R.Acc+100

--- gui_wsdist.py
+++ gui_wsdist.py
@@ -347,7 +347,7 @@ class App(tk.Tk):
         # ('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative')
 
         # Build the basic app.
-        self.title("Kastra FFXI Damage Simulator (Beta: 2025 June 21a)") # pyinstaller --exclude-module gear --exclude-module enemies --clean --onefile gui_wsdist.py
+        self.title("Kastra FFXI Damage Simulator (Beta: 2025 June 29a)") # pyinstaller --exclude-module gear --exclude-module enemies --clean --onefile gui_wsdist.py
         self.horizontal = False
         if not self.horizontal:
             self.geometry("700x885")
